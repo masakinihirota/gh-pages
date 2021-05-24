97b2e8e024ee0ae2dbd4a81c810c6d483994f294
masakinihirota <masakinihirota@gmail.com>
2021-05-24 13:51:04 +0900

    Deploy Next.js to gh-pages

---
+++ out/_next/static/7cdIzTupPFxhrFvTRid6M/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-392ce9a7a72f74e78dee.js"],"/_error":["static/chunks/pages/_error-d799e3a96772416696b7.js"],"/about":["static/chunks/pages/about-757c3923bfc5ecb64d66.js"],sortedPages:["/","/_app","/_error","/about"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
